b5f9b5232b3d4d34fce889e94a203c23fbb1b57b
flprs <flprsy@gmail.com>
2025-04-04 14:52:32 +0300

    Kan Bağış Uygulaması V3

--- app/src/main/java/com/beykentuni/kanbagisuygulamasiv3/Register.java
+++ app/src/main/java/com/beykentuni/kanbagisuygulamasiv3/Register.java
@@ -1,6 +1,9 @@
 package com.beykentuni.kanbagisuygulamasiv3;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Register extends AppCompatActivity {
 
+    TextInputEditText editTextEmail,editTextPassword,editTextPassword2;
+    Button registerButton,gobackButton;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+
+        // XML deki Bileşenleri buraya bağladık
+        editTextEmail = findViewById(R.id.editEmail);
+        editTextPassword = findViewById(R.id.registerPassword);
+        editTextPassword2 = findViewById(R.id.registerPassword2);
+        registerButton = findViewById(R.id.registerButton);
+        gobackButton=findViewById(R.id.goBackButton);
+
+        // Kullanıcı Oluştur butonuna basma eylemi ekledik
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // String email ve password değişkeni oluşturduk
+                // bu değişkenleri tasarımda mail ve şifre giriş yerine bağladık
+                String email,password;
+                email = editTextEmail.getText().toString();
+                password =editTextPassword.getText().toString();
+            }
         });
+        }
     }
-}
+
